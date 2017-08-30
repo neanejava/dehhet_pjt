c0e821476705831c3b32ee33dfd69e440c3f7c4a
lws <lws@DESKTOP-3HRDD4K>
2017-08-30 22:19:13 +0900

    이원식 -홈콘트롤러 수정

--- dehet/src/main/java/com/dehhet/app/HomeController.java
+++ dehet/src/main/java/com/dehhet/app/HomeController.java
@@ -31,7 +31,8 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		
-		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("serverDate", formattedDate );
+		model.addAttribute("abc", 123);
 		
 		return "home";
 	}
