fafb7189515b5349e114c4a312954912e0503a76
lws <lws@DESKTOP-3HRDD4K>
2017-08-30 22:13:59 +0900

    이원식 풀

--- dehet/src/main/java/com/dehhet/app/HomeController.java
+++ dehet/src/main/java/com/dehhet/app/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Handles requests for the application home page.
+ * 이원식이 수정합니다.
  */
 @Controller
 public class HomeController {
